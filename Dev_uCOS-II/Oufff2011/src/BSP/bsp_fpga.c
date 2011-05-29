30cb16090bce0b4f9b3dde06690fe2b7da5a1a64
pcrack <pcrack@896826c9-5d15-7ee0-5ff1-075eea20b736>
2011-05-29 20:57:47 +0000

    AX12: BSP MaJ AX12

--- Dev_uCOS-II/Oufff2011/src/BSP/bsp_fpga.c
+++ Dev_uCOS-II/Oufff2011/src/BSP/bsp_fpga.c
@@ -24,7 +24,13 @@ void AX12_UART_Init (void)
  // · Initialize the Baud Rate Divisor Registers to set the desired transfer rate
  // · Reinitialize the LCR register (with bit 7 = 0)
  // · Set the FIFO trigger level (in the FCR register)
- //· Enable the desired interrupts
+ // · Enable the desired interrupts
+
+	PMP_Write(AX12_UART_LCR, 0x83); // Ob1OOOOO11
+	PMP_Write(AX12_UART_DLL, 0x0B);
+	PMP_Write(AX12_UART_DLM, 0x00);
+	PMP_Write(AX12_UART_LCR, 0x03); // Ob0OOOOO11
+//	PMP_Write(AX12_UART_FCR, 0x00);
 }
 
 CPU_INT08U AX12_UART_Read_Status (void)
@@ -62,7 +68,124 @@ void AX12_UART_Write (CPU_INT08U data)
 	PMP_Write(AX12_UART_FIFO, data);
 }
 
+float gg;
+CPU_INT08U AX12_Read_Return_Packet()
+{
+	CPU_INT08U timeout;
+	CPU_INT08U id=0, length=0, error=0xFF, checksum=0, trash=0;
+
+	char * buffer_ptr;
+	char uart_buffer[8];
+
+	timeout=250; // OxFF
+	while((AX12_UART_Data_Ready()!=1) && timeout>0) { timeout--; BSP_Dly(100); }
+	if(timeout==0) return 0xFF;
+	else trash=AX12_UART_Read();
+
+	timeout=250; // OxFF
+	while((AX12_UART_Data_Ready()!=1) && timeout>0)	{ timeout--; BSP_Dly(100); }
+	if(timeout==0) return 0xFF;
+	else trash=AX12_UART_Read();
+
+	timeout=250; // ID
+	while((AX12_UART_Data_Ready()!=1) && timeout>0)	{ timeout--; BSP_Dly(100); }
+	if(timeout==0) return 0xFF;
+	else id=AX12_UART_Read();
+	
+	timeout=250; // LENGTH
+	while((AX12_UART_Data_Ready()!=1) && timeout>0)	{ timeout--; BSP_Dly(100); }
+	if(timeout==0) return 0xFF;
+	else length=AX12_UART_Read();
+
+	timeout=250; // ERROR
+	while((AX12_UART_Data_Ready()!=1) && timeout>0)	{ timeout--; BSP_Dly(100); }
+	if(timeout==0) return 0xFF;
+	else error=AX12_UART_Read();
+
+	timeout=250; // CHECKSUM
+	while((AX12_UART_Data_Ready()!=1) && timeout>0)	{ timeout--; BSP_Dly(100); }
+	if(timeout==0) return 0xFF;
+	else checksum=AX12_UART_Read();
+
+	return error;
+}
+
 // AX12 CONTROL FUNCTIONS
+
+CPU_INT08U AX12_Write_Position (CPU_INT08U ax12_id, CPU_INT08U setpoint_high, CPU_INT08U setpoint_low)
+{
+	CPU_INT16U checksum;
+	CPU_INT08U length=5;
+	CPU_INT08U status=0xAA;
+
+	checksum = ~(ax12_id + length + AX12_CMD_WRITE_DATA + AX12_REG_GOAL_POSITION + setpoint_low + setpoint_high);
+
+    AX12_UART_Write (0xFF);
+	AX12_UART_Write (0xFF);
+	AX12_UART_Write (ax12_id); // ID
+	AX12_UART_Write (length); // LENGTH
+	AX12_UART_Write (AX12_CMD_WRITE_DATA); // INSTRUCTION
+	AX12_UART_Write (AX12_REG_GOAL_POSITION); // Reg address
+	AX12_UART_Write (setpoint_low);
+	AX12_UART_Write (setpoint_high);
+	AX12_UART_Write ( (CPU_INT08U) checksum); // CHECKSUM
+
+	status=AX12_Read_Return_Packet();
+
+	return status;
+}
+
+void AX12_Write_Position_Sync (CPU_INT08U ax12_id1, CPU_INT08U ax12_id2, CPU_INT08U setpoint1_high, CPU_INT08U setpoint1_low, CPU_INT08U setpoint2_high, CPU_INT08U setpoint2_low)
+{
+	CPU_INT16U checksum;
+	CPU_INT08U L=2;
+	CPU_INT08U length=(2*(L+1))+4;
+
+	checksum = ~(0xFE + length + AX12_CMD_SYNC_WRITE + AX12_REG_GOAL_POSITION + L + ax12_id1 + setpoint1_low + setpoint1_high + ax12_id2 + setpoint2_low + setpoint2_high);
+
+    AX12_UART_Write (0xFF); 
+	AX12_UART_Write (0xFF);
+	AX12_UART_Write (0xFE); // ID BROADCAST
+	AX12_UART_Write (length); // LENGTH
+	AX12_UART_Write (AX12_CMD_SYNC_WRITE); // INSTRUCTION
+
+	AX12_UART_Write (AX12_REG_GOAL_POSITION); // Starting address where the data is written
+	AX12_UART_Write (L); // LENGTH PER AX
+
+	AX12_UART_Write (ax12_id1); // ID1
+	AX12_UART_Write (setpoint1_low);
+	AX12_UART_Write (setpoint1_high);
+
+	AX12_UART_Write (ax12_id2); // ID2
+	AX12_UART_Write (setpoint2_low);
+	AX12_UART_Write (setpoint2_high);
+
+	AX12_UART_Write ( (CPU_INT08U) checksum); // CHECKSUM
+
+	while(AX12_UART_Data_Sent()!=1);
+}
+
+CPU_INT08U AX12_Write_Reg (CPU_INT08U ax12_id, CPU_INT08U reg_address, CPU_INT08U data)
+{
+	CPU_INT16U checksum;
+	CPU_INT08U length=4;
+	CPU_INT08U status=0xAA;
+
+	checksum = ~(ax12_id + length + AX12_CMD_WRITE_DATA + reg_address + data);
+
+    AX12_UART_Write (0xFF);
+	AX12_UART_Write (0xFF);
+	AX12_UART_Write (ax12_id); // ID
+	AX12_UART_Write (length); // LENGTH
+	AX12_UART_Write (AX12_CMD_WRITE_DATA); // INSTRUCTION
+	AX12_UART_Write (reg_address); // Reg address
+	AX12_UART_Write (data);
+	AX12_UART_Write ( (CPU_INT08U) checksum); // CHECKSUM
+
+	status=AX12_Read_Return_Packet();
+
+	return status;
+}
 
 CPU_INT08U AX12_Read_status (CPU_INT08U ax12_id)
 {
@@ -76,28 +199,58 @@ CPU_INT08U AX12_Read_status (CPU_INT08U ax12_id)
 	AX12_UART_Write (0x02); // LENGTH
 	AX12_UART_Write (AX12_CMD_PING); // INSTRUCTION
 	AX12_UART_Write (0xFB); // CHECKSUM
-	while(AX12_UART_Data_Sent()!=1);
 
-	for(i=0; i<6; i++)
-	{
-		while(AX12_UART_Data_Ready()!=1);
-
-		if(i==4) status=AX12_UART_Read();
-		else AX12_UART_Read();
-	}
+	status=AX12_Read_Return_Packet();
 
 	return status;
 }
 
-/*void AX12_Write (CPU_INT8U address_ax12, CPU_INT8U address_reg, CPU_INT8U data)
+void HOLDER_Open(void)
 {
-	PMP_Write(AX12_TX,0xFF);
-
-	while(PMP_Read(QUAD_LATCH_REGISTER)!=0x00);
-
-	PMP_Write(RESET_REGISTER,0xFF);
+	AX12_Write_Position_Sync(AX12_FL_ID, AX12_FR_ID, 0x01, 0x9A, 0x02, 0x6C);
+//	AX12_Write_Position(AX12_FL_ID,0x01,0x9A);
+//	AX12_Write_Position(AX12_FR_ID,0x02,0x6C);
 }
-*/
+
+void HOLDER_Close(void)
+{
+	AX12_Write_Position_Sync(AX12_FL_ID, AX12_FR_ID, 0x02, 0x24, 0x01, 0xD5);
+//	AX12_Write_Position(AX12_FL_ID,0x02,0x24);
+//	AX12_Write_Position(AX12_FR_ID,0x01,0xD5);
+}
+
+void HOLDER_Level_Low(void)
+{
+	AX12_Write_Position_Sync(AX12_TL_ID, AX12_TR_ID, 0x00, 0x32, 0x03, 0xFF);
+}
+
+void HOLDER_Level_Middle(void)
+{
+	AX12_Write_Position_Sync(AX12_TL_ID, AX12_TR_ID, 0x02, 0x00, 0x02, 0x00);
+}
+
+void HOLDER_Level_High(void)
+{
+	AX12_Write_Position_Sync(AX12_TL_ID, AX12_TR_ID, 0x03, 0xFF, 0x00, 0x32);
+}
+
+void HOLDER_Init(void)
+{
+	// ALARM Shutdown
+	AX12_Write_Reg(AX12_FL_ID, AX12_REG_ALARM_SHUTDOWN, 0x04);
+	AX12_Write_Reg(AX12_FR_ID, AX12_REG_ALARM_SHUTDOWN, 0x04);
+//	AX12_Write_Reg(AX12_TL_ID, AX12_REG_ALARM_SHUTDOWN, 0x00);
+	AX12_Write_Reg(AX12_TR_ID, AX12_REG_ALARM_SHUTDOWN, 0x04);
+
+	AX12_Write_Reg(AX12_FL_ID, AX12_REG_TEMPERATURE_LIMIT, 0x55);
+	AX12_Write_Reg(AX12_FR_ID, AX12_REG_TEMPERATURE_LIMIT, 0x55);
+//	AX12_Write_Reg(AX12_TL_ID, AX12_REG_TEMPERATURE_LIMIT, 0x55);
+	AX12_Write_Reg(AX12_TR_ID, AX12_REG_TEMPERATURE_LIMIT, 0x55);
+
+	// HOLDER Init
+	HOLDER_Level_Low();
+}
+
 void QUAD_Latch (void)
 {
 	PMP_Write(QUAD_LATCH_REGISTER,0xFF); // Latch quadrature counters
@@ -133,7 +286,36 @@ CPU_INT08U COLOR_Read (void)
 
 void BSPFPGA_Reset(void)
 {
+
+	char test;
+
 	PMP_Write(RESET_REGISTER,0xFF);
 	PMP_Write(RESET_REGISTER,0x00);
 	PMP_Write(LED_REGISTER,0x01);
+
+	AX12_UART_Init ();
+
+	HOLDER_Init();
+
+/*	HOLDER_Open();
+	BSP_Dly(100000);
+	HOLDER_Close();
+
+	BSP_Dly(200000);
+	HOLDER_Level_Middle();
+
+	BSP_Dly(200000);
+	HOLDER_Level_Low();
+
+	BSP_Dly(200000);
+	HOLDER_Level_High();
+
+	BSP_Dly(200000);
+	HOLDER_Level_Middle();
+
+	BSP_Dly(200000);
+	HOLDER_Level_Low();
+
+	BSP_Dly(200000);
+	HOLDER_Open();*/
 }
--- Dev_uCOS-II/Oufff2011/src/BSP/bsp_fpga.h
+++ Dev_uCOS-II/Oufff2011/src/BSP/bsp_fpga.h
@@ -16,16 +16,20 @@
 #define  QUADG_LO_REGISTER          0x000B
 #define  QUADG_HI_REGISTER          0x000C
 #define  GPIO_REGISTER		        0x0010
-#define  AX12_UART_FIFO					0x0100 // RBR + THR registers
-#define  AX12_UART_STATUS				0x0102 // LSR regsiter
+
+#define  AX12_UART_FIFO				0x0020 // RBR + THR registers
+#define  AX12_UART_STATUS			0x0025 // LSR regsiter
+#define  AX12_UART_LCR				0x0023 
+#define  AX12_UART_DLL				0x0020 
+#define  AX12_UART_DLM				0x0021 
 
 #define  AX12_UART_MASK_DATA_READY  0x01 // Data ready bit (=1 if data received in FIFO)
 #define  AX12_UART_MASK_DATA_SENT   0x40 // Transmitter empty bit (=1 if buffer is empty)
 
-#define  AX12_FL_ID			        0x01
-#define  AX12_FR_ID			        0x02
-#define  AX12_TL_ID			        0x03
-#define  AX12_TR_ID			        0x04
+#define  AX12_FL_ID			        0x0F
+#define  AX12_FR_ID			        0x12
+#define  AX12_TL_ID			        0x10
+#define  AX12_TR_ID			        0x11
 
 #define  AX12_CMD_PING				0x01
 #define  AX12_CMD_READ_DATA         0x02
@@ -42,6 +46,10 @@
 #define AX12_ERROR_MASK_OVERHEATING 0x04
 #define AX12_ERROR_MASK_ANGLE		0x02
 #define AX12_ERROR_MASK_VOLTAGE		0x01
+
+#define AX12_REG_GOAL_POSITION 		0x1E
+#define AX12_REG_ALARM_SHUTDOWN		0x12
+#define AX12_REG_TEMPERATURE_LIMIT	0x0B
 
 
 
