1130ce7d6d0bd2876e1c77c10345bb62cc83b133
philippe.bechet <philippe.bechet@896826c9-5d15-7ee0-5ff1-075eea20b736>
2012-01-04 10:58:40 +0000

    Mise à jour du projet StrategyGenerator

---
+++ trunk/Info/StrategyGenerator/Codage/FileManager/StructuredFileKey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyGenerator.FileManager
+{
+    class StructuredFileKey
+    {
+         // Constructeur ---------------------------------------------------------------------------
+        /// <summary>
+        /// GroupID : ID used to identify multi keys for one unique pattern line
+        /// Name    : Name of the key to add
+        /// Value   : Value of this key
+        /// </summary>
+        public StructuredFileKey(int GroupID, String Name, String Value)
+        {
+            // Assign current value
+            _GID = GroupID;
+            _Name = Name;
+            _Value = Value;
+
+        }
+
+        // Private --------------------------------------------------------------------------------
+        private int _GID;
+        private String _Name;
+        private String _Value;
+    }
+}
