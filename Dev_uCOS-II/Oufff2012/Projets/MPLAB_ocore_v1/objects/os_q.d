ffc22fa2588b8c3fafa81251e37f010cc0e21ff3
philippe.bechet <philippe.bechet@896826c9-5d15-7ee0-5ff1-075eea20b736>
2012-05-13 22:19:34 +0000

    Ajout des fichiers projets pour la carte de Dev

--- Dev_uCOS-II/Oufff2012/Projets/MPLAB_ocore_v1/objects/os_q.d
+++
@@ -1,5 +0,0 @@
-./objects\os_q.o: ..\..\src\uCOS-II\Source\os_q.c \
-  ../../Src/uCOS-II/Source/ucos_ii.h ../../Src/Main/AppConfig.h \
-  ../../Src/Main/os_cfg.h \
-  ../../Src/uCOS-II/Ports/MIPS32-4K/Vectored-Interrupt/MPLAB-PIC32-GCC/os_cpu.h \
-  ../../Src/Microchip/PeripheralsLibs/INT.h
