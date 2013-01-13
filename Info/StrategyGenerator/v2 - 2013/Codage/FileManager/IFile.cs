5a5e220820e3a86e687a25e94370443ab8b0293c
philippe.bechet <philippe.bechet@gmail.com>
2013-01-13 23:25:03 +0000

    Sauvegarde StrategyGenerator2

---
+++ Info/StrategyGenerator/v2 - 2013/Codage/FileManager/IFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyGenerator2.FileManager
+{
+    interface IFile
+    {
+        /// <summary>
+        /// Charge un fichier texte dont le nom est contenu dans 'fileName'
+        /// </summary>
+        /// <param name="fileName">Chemin d'accès au fichier</param>
+        /// <returns>Nombre de lignes chargées (-1 en cas d'erreur)</returns>
+        int Load(String fileName);
+
+        /// <summary>
+        /// Enregistre le contenu du fichier dans le nom de fichier précédemment utilisé
+        /// </summary>
+        /// <returns>Nombre de lignes écrites (-1 en cas d'erreur)</returns>
+        int Save();
+
+        /// <summary>
+        /// Enregistre un fichier texte au chemin d'accès 'newFileName'. 
+        /// Une fois le fichier enregisté le nom de fichier 'newFileName' utilisé est mis à jour
+        /// </summary>
+        /// <param name="fileName">Chemin d'accès au fichier</param>
+        /// <returns>Nombre de lignes écrites (-1 en cas d'erreur)</returns>
+        int SaveTo(String newFileName);
+    }
+}
---
+++ Info/StrategyGenerator/v2 - 2013/Codage/Model/MainModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StrategyGenerator2.FileManager;
+
+namespace StrategyGenerator2.Model
+{
+    class MainModel
+    {
+        // Public ---------------------------------------------------------------------------------
+        public static MainModel Instance
+        {
+            get
+            {
+                if (_model == null)
+                {
+                    _model = new MainModel();
+                }
+                return _model;
+            }
+        }
+
+
+        // Private --------------------------------------------------------------------------------
+        // Declaration du singleton
+        private static MainModel _model;
+
+        // Constructeur privé (dû au singleton)
+        private MainModel()
+        {
+            // Test !! ########################################################
+            TextFile FichierTest = new TextFile();
+            FichierTest.Load("D:/Robotique/Info/StrategyGenerator/v2 - 2013/Test/TestLoad.txt");
+            FichierTest.Save();
+            FichierTest.SaveTo("D:/Robotique/Info/StrategyGenerator/v2 - 2013/Test/TestLoadOutput.txt");
+
+            // ################################################################
+
+            return;
+        }
+
+    }
+}
