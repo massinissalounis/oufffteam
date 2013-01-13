1017f8f7e95d01a66a2c6ff370b1000b66998701
philippe.bechet@gmail.com <philippe.bechet@gmail.com@896826c9-5d15-7ee0-5ff1-075eea20b736>
2013-01-13 12:45:44 +0000

    Ajout de la structure de fichier pour le projet 2013

---
+++ trunk/Info/StrategyGenerator/v2 - 2013/Codage/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using StrategyGenerator2.ViewModel;
+
+namespace StrategyGenerator2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
