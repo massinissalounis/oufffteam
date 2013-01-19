50ab4d57a2eaccfa451af2a8a6995264c9ca9619
philippe.bechet@gmail.com <philippe.bechet@gmail.com@896826c9-5d15-7ee0-5ff1-075eea20b736>
2013-01-19 17:31:48 +0000

    MAJ du dossier de projet + création du projet de test pour FileManager

---
+++ trunk/Info/StrategyGenerator/v2 - 2013/Test/Tester_FileManager/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using StrategyGenerator2.FileManager;
+
+namespace FileManagerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration des variables
+
+
+            // Test 1 : Creation d'un fichier texte 
+            try
+            {
+                Console.WriteLine("Test 1 - Creation d'un fichier texte :");
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Test 1 Failed !!");
+                Console.WriteLine(ex.Message);
+            }
+
+            // Fin du programme
+            DisplaySeparator();
+            Console.WriteLine("\nTest terminé.");
+            Console.Read();
+        }
+
+        static private void DisplaySeparator()
+        {
+            Console.WriteLine("------------------------------------------------------------------------------");
+        }
+    }
+}
