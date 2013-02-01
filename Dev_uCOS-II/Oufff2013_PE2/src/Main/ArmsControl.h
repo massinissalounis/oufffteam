8283fecd0fbfa774a928f3d9d9b98474519cdd1b
pcrack <pcrack@896826c9-5d15-7ee0-5ff1-075eea20b736>
2013-02-01 13:50:03 +0000

    PIC 2013 : Modifs PE

---
+++ Dev_uCOS-II/Oufff2013_PE2/src/Main/ArmsControl.h
@@ -0,0 +1,38 @@
+// Control for the arms - Oufffteam 2012
+// Pierre - Emmanuel GAILLARDON
+
+#ifndef  ARMSCONTROL_H
+#define  ARMSCONTROL_H
+
+/*
+*********************************************************************************************************
+*                                                DEFINES
+*********************************************************************************************************
+*/
+#define AX12_LEFT_ARM_ID			(0x0B)
+#define AX12_RIGHT_ARM_ID			(0x0A)
+
+/*
+*********************************************************************************************************
+*                                             PROTOTYPES
+*********************************************************************************************************
+*/   
+
+// ARMS ---------------------------------------------------------------------------------
+void ARMS_InitReg(void);
+void ARMS_SetSpeed(void);
+void ARMS_Open(void);
+void ARMS_Close(void);
+void ARMS_Push(void);
+
+// ARM Left -----------------------------------------------------------------------------
+void ARM_Left_Open(void);
+void ARM_Left_Close(void);
+void ARM_Left_Push(void);
+
+// ARM Right ----------------------------------------------------------------------------
+void ARM_Right_Open(void);
+void ARM_Right_Close(void);
+void ARM_Right_Push(void);
+
+#endif // ARMSCONTROL_H
