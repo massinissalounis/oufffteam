faa0c58db7b93fbecad4f7ec043b8d7a02fa3d21
philippe.bechet@gmail.com <philippe.bechet@gmail.com@896826c9-5d15-7ee0-5ff1-075eea20b736>
2013-04-18 20:59:34 +0000

    Sauvegarde du projet "v2 - 2013"

---
+++ trunk/Info/StrategyGenerator/v2 - 2013/Codage/ViewModel/MainStrategyRobot1VM.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+using StrategyGenerator2.Model;
+using System.Windows;
+using StrategyGenerator2.View;
+using System.Collections.ObjectModel;
+using StrategyGenerator2.StrategyManager;
+using System.Collections.Generic;
+
+namespace StrategyGenerator2.ViewModel
+{
+    public class MainStrategyRobot1VM : MainStrategyVM
+    {
+        // Public ---------------------------------------------------------------------------------
+        public MainStrategyRobot1VM()
+        {
+            _mainModel = MainModel.GetInstance;
+
+            if (_mainModel != null)
+                _currentStrategy = _mainModel.strategyRobot1;
+        }
+
+
+
+        // Properties -----------------------------------------------------------------------------
+
+        // Private --------------------------------------------------------------------------------
+    }
+}
