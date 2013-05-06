651636e1cd8220841ea492d79b12f3e71f8bd849
philippe.bechet <philippe.bechet@gmail.com>
2013-05-06 00:25:05 +0000

    Sauvegarde du projet "v2 - 2013"

---
+++ Info/StrategyGenerator/v2 - 2013/Codage/StrategyViewer/SubObjectStrategyViewer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyGenerator2.StrategyViewer
+{
+    public class RobotStatusFlag
+    {
+        public RobotStatusFlag()
+        {
+            _hoopStatus = EnumRobotStatusFlag.Closed;
+            _leftArmStatus = EnumRobotStatusFlag.Closed;
+            _rightArmStatus = EnumRobotStatusFlag.Closed;
+        }
+
+        public RobotStatusFlag(RobotStatusFlag newValue)
+        {
+            if(newValue != null)
+            {
+                HoopStatus = newValue.HoopStatus;
+                LeftArmStatus = newValue.LeftArmStatus;
+                RightArmStatus = newValue.RightArmStatus;
+            }
+            else
+            {
+                _hoopStatus = EnumRobotStatusFlag.Closed;
+                _leftArmStatus = EnumRobotStatusFlag.Closed;
+                _rightArmStatus = EnumRobotStatusFlag.Closed;
+            }
+        }
+
+        // Public ---------------------------------------------------------------------------------
+        public EnumRobotStatusFlag HoopStatus
+        {
+            get { return _hoopStatus; }
+            set
+            {
+                if (value != null)
+                {
+                    if (value == EnumRobotStatusFlag.Open)
+                        _hoopStatus = value;
+                    else
+                        _hoopStatus = EnumRobotStatusFlag.Closed;
+                }
+            }
+        }
+        public EnumRobotStatusFlag LeftArmStatus
+        {
+            get { return _leftArmStatus; }
+            set
+            {
+                if (value != null)
+                {
+                    if ((value == EnumRobotStatusFlag.Open) || (value == EnumRobotStatusFlag.Closed) || (value == EnumRobotStatusFlag.Front))
+                        _leftArmStatus = value;
+                    else
+                        _leftArmStatus = EnumRobotStatusFlag.Closed;
+                }
+            }
+        }
+        public EnumRobotStatusFlag RightArmStatus
+        {
+            get { return _rightArmStatus; }
+            set
+            {
+                if (value != null)
+                {
+
+                    if ((value == EnumRobotStatusFlag.Open) || (value == EnumRobotStatusFlag.Closed) || (value == EnumRobotStatusFlag.Front))
+                        _rightArmStatus = value;
+                    else
+                        _rightArmStatus = EnumRobotStatusFlag.Closed;
+                }
+            }
+        }
+
+        // Private --------------------------------------------------------------------------------
+        private EnumRobotStatusFlag _hoopStatus = EnumRobotStatusFlag.Closed;
+        private EnumRobotStatusFlag _leftArmStatus = EnumRobotStatusFlag.Closed;
+        private EnumRobotStatusFlag _rightArmStatus = EnumRobotStatusFlag.Closed;
+    }
+
+    public enum EnumRobotStatusFlag
+    {
+        Closed = 1,
+        Open = 2,
+        Front = 4,
+    }
+}
---
+++ Info/StrategyGenerator/v2 - 2013/Codage/ViewModel/HelperVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+using StrategyGenerator2.Model;
+
+namespace StrategyGenerator2.ViewModel
+{
+    public class HelperVM : ViewModelBase
+    {
+        public HelperVM()
+        {
+            _mainModel = MainModel.GetInstance;
+            _mainModel.EUpdateCmdDescription += new EventHandler(UpdateData);
+
+        }
+
+        public String HelperDescription
+        {
+            get
+            {
+                if (_mainModel != null)
+                    return _mainModel.RobotActionDescription;
+                else
+                    return "";
+            }
+            set { }
+        }
+
+        // Private --------------------------------------------------------------------------------
+        protected MainModel _mainModel = null;                      // Lien vers le model
+
+        private void UpdateData(object sender, EventArgs e)
+        {
+            RaisePropertyChanged("HelperDescription");
+        }
+    }
+}
